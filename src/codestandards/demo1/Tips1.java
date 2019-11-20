b8eaa6dc3350895fc9fa19d3db29bd196ebbcda0
xzw <2580001734@qq.com>
2019-11-20 19:07:03 +0800

    3

--- src/codestandards/demo1/Tips1.java
+++ src/codestandards/demo1/Tips1.java
@@ -1,8 +1,10 @@
 package demo1;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -16,5 +18,28 @@ public class Tips1 {
         list.contains("");
         Set<Object> set = new HashSet<>();
         set.contains("");
+
+        String str = "aa.txt";
+        str.split(".");
+        // Java编译器中的特殊字符都要重新转义-
+
+
     }
+    private static Map<String, Integer> map = new HashMap<String, Integer>(){
+        {
+            map.put("Leo",1);
+            map.put("Family-loving",2);
+            map.put("Cold on the out side passionate on the inside",3);
+        }
+    };
+    private static List<String> list = new ArrayList<String>(){
+        {
+            list.add("Sagittarius");
+            list.add("Charming");
+            list.add("Perfectionist");
+        }
+    };
+
+
+
 }
