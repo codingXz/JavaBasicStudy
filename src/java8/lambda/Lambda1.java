2cd6664de0d1cb21adb1690080240d60e2a213ce
xzw <2580001734@qq.com>
2020-01-09 10:34:50 +0800

    1.添加junit 2.调整目录

---
+++ src/java8/lambda/Lambda1.java
@@ -0,0 +1,14 @@
+package lambda;
+
+import org.junit.Test;
+
+/**
+ * @Author: xzw
+ * @Date: 2020/1/9
+ */
+public class Lambda1 {
+    @Test
+    public void t1() {
+        System.out.println("xxzw");
+    }
+}
--- src/standard/demo/Tips1.java
+++ src/standard/demo/Tips1.java
@@ -1,4 +1,4 @@
-package demo1;
+package demo;
 
 import java.util.ArrayList;
 import java.util.HashMap;
